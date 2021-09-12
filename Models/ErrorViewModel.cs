ff2e28a6139d49b3c11f09376464f649a368f534
J C <jay-c_117@hotmail.co.uk>
2021-09-12 21:21:20 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Example_MVC_Application_To_Try_Azure_Devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
